00ecdafd1e572de269d3b9688b58b7007a6d09c8
So-Xtina <mu7zic2soul@gmail.com>
2018-06-05 15:26:43 -0700

    completed redux set-up

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={createStore(rootReducer)}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById("root")
+);
+
 registerServiceWorker();
