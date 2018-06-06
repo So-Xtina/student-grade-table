d0a3779f4b9b105714e5308b42160cfc0d40672b
So-Xtina <mu7zic2soul@gmail.com>
2018-06-06 16:11:03 -0700

    created endpoints for the student data

---
+++ routes/get_student_data.js
@@ -0,0 +1,27 @@
+module.exports = (webserver, mysql, database) => {
+	webserver.get("/api/get_student_data", (req, res) => {
+		const output = {
+			success: false,
+			data: [],
+			errors: []
+		};
+
+		let query = `SELECT 
+            id, 
+            first_name, 
+            last_name
+        FROM users`;
+
+		database.query(query, (err, data, fields) => {
+			if (!err) {
+				output.success = true;
+				output.data = data;
+				output.message = "Query was successful";
+			} else {
+				output.errors = err;
+			}
+
+			res.json(output);
+		});
+	});
+};
