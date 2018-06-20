bf6422ca56ffca51c4760daa5c74ac7c87dd23dd
So-Xtina <mu7zic2soul@gmail.com>
2018-06-20 15:25:29 -0700

    created and in progress front end input validation, so far have half of it done for the front end in terms of the edit modal

--- client/src/components/edit_student.js
+++ client/src/components/edit_student.js
@@ -1,7 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class EditStudent extends Component{
-
+class EditStudent extends Component {
 	//used this to debug and be able to call this.props in the console of the chrome dev once the state has been first rendered, if you want to see the state after the update use componentWillUpdate
 	// componentDidMount() {
 	// 	debugger;
@@ -9,12 +8,19 @@ class EditStudent extends Component{
 
 	render() {
 		const { student_name, class_name, grade_value, id } = this.props.studentObj;
+		const { displayErrors, hasError } = this.props;
 
-		return <div className="editModal" role="dialog">
+		return (
+			<div className="editModal" role="dialog">
 				<div className="modal-dialog">
 					<div className="modal-content">
 						<div className="modal-header">
-							<button onClick={this.props.showEditModal} type="button" className="close" data-dismiss="modal">
+							<button
+								onClick={this.props.showEditModal}
+								type="button"
+								className="close"
+								data-dismiss="modal"
+							>
 								&times;
 							</button>
 							<h4 className="modal-title text-center">Edit Student Info &amp; Grade</h4>
@@ -25,36 +31,80 @@ class EditStudent extends Component{
 									<label htmlFor="inputStudentName3" className="col-sm-2 control-label">
 										Student Name:
 									</label>
-									<div className="col-sm-9">
-										<input onChange={this.props.editModalInputs} ref={id} value={student_name} type="text" className="form-control" id="inputStudentName3" name="student_name" placeholder="Student Name" />
+									<div className={hasError ? "col-sm-9 has-error" : "col-sm-9 has-success"}>
+										<input
+											onChange={this.props.editModalInputs}
+											ref={id}
+											value={student_name}
+											type="text"
+											className="form-control"
+											id="inputStudentName3"
+											name="student_name"
+											placeholder="Student Name"
+										/>
+										<span className={displayErrors ? "" : "hideErrMessage"}>
+											Not a valid input, must contain letters up to 2-50 characters long.
+										</span>
 									</div>
 								</div>
 								<div className="form-group">
 									<label htmlFor="inputStudentCourse3" className="col-sm-2 control-label">
 										Student Course:
 									</label>
-									<div className="col-sm-9">
-										<input onChange={this.props.editModalInputs} ref={id} value={class_name} type="text" className="form-control" id="inputStudentCourse3" name="class_name" placeholder="Student Course" />
+									<div className={hasError ? "col-sm-9 has-error" : "col-sm-9 has-success"}>
+										<input
+											onChange={this.props.editModalInputs}
+											ref={id}
+											value={class_name}
+											type="text"
+											className={hasError ? "form-control has-error" : "form-control has-success"}
+											id="inputStudentCourse3"
+											name="class_name"
+											placeholder="Student Course"
+										/>
+										<span className={displayErrors ? "" : "hideErrMessage"}>
+											Not a valid input, must contain alphanumeric characters from 2-50 characters
+											long for class name.
+										</span>
 									</div>
 								</div>
 								<div className="form-group">
 									<label htmlFor="inputStudentGrade3" className="col-sm-2 control-label">
 										Student Grade:
 									</label>
-									<div className="col-sm-9">
-										<input onChange={this.props.editModalInputs} ref={id} value={grade_value} type="text" className="form-control" id="inputStudentGrade3" name="grade_value" placeholder="Student Grade" />
+									<div className={hasError ? "col-sm-9 has-error" : "col-sm-9 has-success"}>
+										<input
+											onChange={this.props.editModalInputs}
+											ref={id}
+											value={grade_value}
+											type="text"
+											className={hasError ? "form-control has-error" : "form-control has-success"}
+											id="inputStudentGrade3"
+											name="grade_value"
+											placeholder="Student Grade"
+										/>
+										<span className={displayErrors ? "" : "hideErrMessage"}>
+											Not a valid input, must contain only numbers that are 3-4 characters long
+											for the grade value.
+										</span>
 									</div>
 								</div>
 							</form>
 						</div>
 						<div className="modal-footer">
-							<button onClick={this.props.editStudentDataInput} type="button" className="btn btn-info" data-dismiss="modal">
+							<button
+								onClick={this.props.editStudentDataInput}
+								type="button"
+								className="btn btn-info"
+								data-dismiss="modal"
+							>
 								Submit
 							</button>
 						</div>
 					</div>
 				</div>
-			</div>;
+			</div>
+		);
 	}
 }
 
---
+++ client/src/components/input_validation.js
@@ -0,0 +1,29 @@
+export function lettersValidation(value) {
+	const letters = /^[a-z ,.'-]+$/i;
+
+	if (value === "" || !letters.test(value)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+export function numbersValidation(value) {
+	const numbers = /^[0-9 ,.]{2,5}$/;
+
+	if (value === "" || !numbers.test(value)) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+export function numLetValidation(value) {
+	const numberLetters = /^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/;
+
+	if (value === "" || !numberLetters.test(value)) {
+		return false;
+	} else {
+		return true;
+	}
+}
