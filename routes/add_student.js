02057df78d8c567647230b15201644a080993736
So-Xtina <mu7zic2soul@gmail.com>
2018-07-19 19:53:52 -0700

    did not finish catch-err, but did complete backend input validation

--- routes/add_student.js
+++ routes/add_student.js
@@ -1,42 +1,65 @@
+const { check, validationResult } = require("express-validator/check");
+const letters = /^[a-z ,.'-]+$/i;
+const numberLetters = /^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/;
+
 module.exports = (webserver, mysql, database) => {
-	webserver.post("/api/add_student", (req, res) => {
-		const output = {
-			success: false,
-			data: [],
-			errors: [],
-			message: ""
-		};
+	webserver.post(
+		"/api/add_student",
+		[
+			check("class_name")
+				.isLength({ min: 1, max: 50 })
+				.matches(numberLetters),
+			check("grade_value")
+				.isLength({ min: 1 })
+				.isDecimal({ force_decimal: false, decimal_digits: "1,3", locale: "en-US" }),
+			check("student_name")
+				.isLength({ min: 1, max: 100 })
+				.matches(letters)
+		],
+		(req, res) => {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(422).json({ errors: errors.array() });
+			}
 
-		//if field names are variable OR if field name is provided by front end, reason to do this is to prevent injection attacks;
-		// let query = `
-		// 	INSERT INTO ??
-		// 	(??, ??, ??)
-		// 	VALUES (?, ?, ?)
-		// `;
-		// let inserts = ['grades', 'class_name', 'student_name', 'grade_value', class_name, student_name, grade_value];
+			const output = {
+				success: false,
+				data: [],
+				errors: [],
+				message: ""
+			};
 
-		let { class_name, grade_value, student_name } = req.body;
+			//if field names are variable OR if field name is provided by front end, reason to do this is to prevent injection attacks;
+			// let query = `
+			// 	INSERT INTO ??
+			// 	(??, ??, ??)
+			// 	VALUES (?, ?, ?)
+			// `;
+			// let inserts = ['grades', 'class_name', 'student_name', 'grade_value', class_name, student_name, grade_value];
 
-		let query = `
+			let { class_name, grade_value, student_name } = req.body;
+
+			let query = `
 			INSERT INTO grades
 			(class_name, student_name, grade_value)
 			VALUES (?, ?, ?)
         `;
 
-		let inserts = [class_name, student_name, grade_value];
+			let inserts = [class_name, student_name, grade_value];
 
-		let mysqlQuery = mysql.format(query, inserts);
+			let mysqlQuery = mysql.format(query, inserts);
 
-		database.query(mysqlQuery, (err, data, fields) => {
-			if (!err) {
-				output.success = true;
-				output.data = data;
-				output.message = "Query successful";
-			} else {
-				output.errors = err;
-				output.message = "Query unsuccessful";
-			}
-			res.json(output);
-		});
-	});
+			database.query(mysqlQuery, (err, data, fields) => {
+				if (!err) {
+					output.success = true;
+					output.data = data;
+					output.message = "Query successful";
+				} else {
+					output.errors = err;
+					output.message = "Query unsuccessful";
+				}
+				res.json(output);
+			});
+		}
+	);
 };
--- routes/edit_student.js
+++ routes/edit_student.js
@@ -1,34 +1,57 @@
+const { check, validationResult } = require("express-validator/check");
+const letters = /^[a-z ,.'-]+$/i;
+const numberLetters = /^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/;
+
 module.exports = (webserver, mysql, database) => {
-	webserver.put("/api/edit_student_data", (req, res) => {
-		const output = {
-			success: false,
-			data: [],
-			errors: [],
-			message: ""
-		};
+	webserver.put(
+		"/api/edit_student_data",
+		[
+			check("class_name")
+				.isLength({ min: 1, max: 50 })
+				.matches(numberLetters),
+			check("grade_value")
+				.isLength({ min: 1 })
+				.isDecimal({ force_decimal: false, decimal_digits: "1,3", locale: "en-US" }),
+			check("student_name")
+				.isLength({ min: 1, max: 100 })
+				.matches(letters)
+		],
+		(req, res) => {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(422).json({ errors: errors.array() });
+			}
 
-		let { class_name, student_name, grade_value, id } = req.body.student;
+			const output = {
+				success: false,
+				data: [],
+				errors: [],
+				message: ""
+			};
 
-		let query = `
+			let { class_name, student_name, grade_value, id } = req.body.student;
+
+			let query = `
             UPDATE grades SET class_name = ?, student_name = ?, grade_value = ?
             WHERE id = ?
         `;
 
-		let inserts = [class_name, student_name, grade_value, id];
+			let inserts = [class_name, student_name, grade_value, id];
 
-		let mysqlQuery = mysql.format(query, inserts);
+			let mysqlQuery = mysql.format(query, inserts);
 
-		database.query(mysqlQuery, (err, data, fields) => {
-			if (!err) {
-				output.success = true;
-				output.data = data;
-				output.message = "Student was successfully updated";
-			} else {
-				output.errors = err;
-				output.message = "Student was not updated";
-			}
+			database.query(mysqlQuery, (err, data, fields) => {
+				if (!err) {
+					output.success = true;
+					output.data = data;
+					output.message = "Student was successfully updated";
+				} else {
+					output.errors = err;
+					output.message = "Student was not updated";
+				}
 
-			res.json(output);
-		});
-	});
+				res.json(output);
+			});
+		}
+	);
 };
